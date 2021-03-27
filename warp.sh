a7a71f12a30e1c3d9531912c7b6de53a3e84abc3
TonyLCH <37832991+TonyLCH@users.noreply.github.com>
2021-03-27 11:28:35 +0800

    Add files via upload

---
+++ warp.sh
@@ -0,0 +1,17 @@
+apt update
+apt install curl sudo lsb-release -y
+echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
+sudo apt update
+sudo apt install net-tools iproute2 openresolv dnsutils -y
+sudo apt install wireguard-tools --no-install-recommends
+sudo apt -t $(lsb_release -sc)-backports install linux-image-$(dpkg --print-architecture) linux-headers-$(dpkg --print-architecture) --install-recommends -y
+sudo apt install wireguard-dkms -y
+curl -fsSL wireguard-go.hkcls-network.workers.dev | sudo bash
+curl -fsSL wgcf-install.hkcls-network.workers.dev | sudo bash
+wgcf register
+wgcf generate
+sed -i '/\:\:\/0/d' wgcf-profile.conf | sed -i 's/engage.cloudflareclient.com/[2606:4700:d0::a29f:c001]/g' wgcf-profile.conf
+cp wgcf-profile.conf /etc/wireguard/wgcf.conf
+sudo systemctl start wg-quick@wgcf
+sudo systemctl enable wg-quick@wgcf
+grep -qE '^[ ]*label[ ]*2002::/16[ ]*2' /etc/gai.conf || echo 'label 2002::/16   2' | sudo tee -a /etc/gai.conf
